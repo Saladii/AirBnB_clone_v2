e40c63889cc3be1252e60e0e49d5df6e2cefc8c0
Selehadin <selehadinseid1@gmail.com>
2023-07-06 10:56:09 +0000

    jhds

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
+""" do_pack function """
 
-from datetime import datetime
 from fabric.api import local
+from datetime import datetime
 
 
 def do_pack():
+    """ Generates a .tgz archive from the contents of the
+        web_static folder of your AirBnB Clone repository.
     """
-        Return the archive path if archive has been correctly
-        gernerated.
-    """
+
+    date = datetime.now()
+    filename = "web_static_{}{}{}{}{}{}.tgz".format(date.year,
+                                                    date.month,
+                                                    date.day,
+                                                    date.hour,
+                                                    date.minute,
+                                                    date.second)
 
     local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
 
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
+    to_tgz = local("tar -cvzf versions/{} web_static".format(filename))
+
+    if to_tgz.failed:
         return None
+    else:
+        return "versions/{}".format(filename)
